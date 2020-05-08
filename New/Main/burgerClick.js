2bdfe70bb8cdef5c20a45653f48e9f9cfd88e068
Aakartion <56947697+Aakartion@users.noreply.github.com>
2020-05-08 11:01:30 -0500

    Add files via upload

---
+++ New/Main/burgerClick.js
@@ -0,0 +1,34 @@
+const navSlide = () => 
+{
+    const burger =  document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+    
+    
+    burger.addEventListener('click', ()=>
+    {
+        // Toggle Nav
+        nav.classList.toggle('nav-active');
+
+        //Bringing all the list of the nava
+        navLinks.forEach((link, index) => 
+        {
+            if(link.style.animation){
+                link.style.animation="";
+            }
+            else
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index /7  + 0.4}s`;
+        });
+
+        //Burger Animation when closed
+        burger.classList.toggle('toggle');
+
+    });
+}
+
+const main = ()=>
+{
+    navSlide();
+}
+
+main();
